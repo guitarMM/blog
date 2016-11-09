2eb840217e7649a7dd141905ff2a28fc598f7fec
Yui Ezuki <Yui@Yui-no-MacBook-Pro.local>
2016-11-09 19:19:14 +0900

    またコントローラーで誤字見つけたから直した

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    redirect_to post_path(@post.id)
   end
 
   private
