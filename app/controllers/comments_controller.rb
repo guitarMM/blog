5b5f1d3982799acf4bc9983acfc55d900a642acc
Yui Ezuki <Yui@Yui-no-MacBook-Pro.local>
2016-11-14 19:55:34 +0900

    コントローラーいじった、コメント一覧表示できた！

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,4 @@
 class CommentsController < ApplicationController
-  def post:references
-  end
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
