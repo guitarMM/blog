eced6f0ef871043b7deecbc32791aaeec7704ff9
Yui Ezuki <Yui@Yui-no-MacBook-Pro.local>
2016-11-21 20:23:36 +0900

    CSSとhtmlを試しに一気にいじってみた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,17 @@
 class PostsController < ApplicationController
-
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
+
   def show
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     respond_to do |format|
@@ -22,8 +24,10 @@ class PostsController < ApplicationController
       end
     end
   end
+
   def edit
   end
+
   def update
     if @post.update(post_params)
       redirect_to posts_path
@@ -31,18 +35,19 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+
   def destroy
     @post.destroy
     redirect_to posts_path
   end
 
   private
+
   def post_params
     params[:post].permit(:title, :body)
   end
+
   def set_post
     @post = Post.find(params[:id])
   end
-
-
 end
