ba02bdd56cddcc8310c6f51d129bcb50a0ad093e
Yui Ezuki <Yui@Yui-no-MacBook-Pro.local>
2016-12-07 16:18:18 +0900

    名前登録できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
