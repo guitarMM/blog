4c55212b7ec58276a5b9c055ddf88cd76e58b2bf
Yui Ezuki <Yui@Yui-no-MacBook-Pro.local>
2016-12-20 11:24:30 +0900

    変なのって思ってたらやっぱ変なことしてた、メール見る設定訂正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
   }
+
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   root 'posts#index'
@@ -13,12 +15,8 @@ Rails.application.routes.draw do
   get  "posts/save"
   get  "comments/save"
 
-
-  Your::Application.routes.draw do
-  devise_for :users
-    if Rails.env.development?
-      mount LetterOpenerWeb::Engine, at: "/letter_opener"
-    end
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
 end
